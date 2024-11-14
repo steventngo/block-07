deffc752ac1a6c7f36040be6c096da59e9da349c
Steven Ngo <stevenngo@Stevens-MacBook-Pro.local>
2024-11-14 16:42:17 -0600

    secure the vault

---
+++ index.js
@@ -0,0 +1,19 @@
+//Greet the user.  
+// Create a string for the combination.
+alert('You have received this message because you have been chosen to open an important vault. Here is the secret combination:')
+
+//Setup Mathematic Variables as follows
+// 5 x 2 = 10, 30 + 10 = 40, 13 x 3 = 39
+
+
+const codeOne=5;
+const codeTwo=2;
+const codeThree=30;
+const codeFour=10;
+const codeFive=13;
+const codeSix=3;
+console.log(codeOne * codeTwo)
+console.log(codeThree + codeFour)
+console.log(codeFive * codeSix)
+
+
